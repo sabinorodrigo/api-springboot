15cdb5c72a031d338935520c93362b2d37dfdf3f
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2025-09-30 17:11:52 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/lordsabino/api/config/TestConfig.java
+++ src/main/java/br/com/lordsabino/api/config/TestConfig.java
@@ -57,6 +57,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User frodo = new User(null, "Frodo Bolseiro", "frodo@email.com", "15151515", "bolseiro");
         User bilbo = new User(null, "Bilbo Bolseiro", "bilbo@email.com", "16161616", "bolseiro");
 
