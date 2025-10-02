33c033c188ee4b40758a9908a1a23e5fd7921309
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2025-10-02 16:16:44 -0300

    User insert

--- src/main/java/br/com/lordsabino/api/resources/UserResource.java
+++ src/main/java/br/com/lordsabino/api/resources/UserResource.java
@@ -1,15 +1,14 @@
 package br.com.lordsabino.api.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.lordsabino.api.Entities.User;
 import br.com.lordsabino.api.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,5 +30,15 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
